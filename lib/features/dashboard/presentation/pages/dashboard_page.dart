c8adacea6b57d001fca13948ea6652c21dc1627a
rmsh03dhj <dhoju.ramesh03@gmail.com>
2021-07-26 10:32:30 +1000

    wip

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -1,18 +1,13 @@
+import 'dart:io';
+
+import 'package:barahi/features/dashboard/presentation/bloc/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
-import 'package:barahi/core/routes/weather_app_routes.dart';
 import 'package:barahi/core/services/navigation_service.dart';
 import 'package:barahi/core/services/service_locator.dart';
-import 'package:barahi/features/app_start/presentation/bloc/app_start_bloc.dart';
-import 'package:barahi/features/app_start/presentation/bloc/app_start_event.dart';
-import 'package:barahi/features/dashboard/presentation/bloc/dashboard.dart';
-import 'package:barahi/features/utils/constants/strings.dart';
-import 'package:barahi/features/utils/widgets/my_app_button.dart';
-import 'package:barahi/features/utils/widgets/my_app_form_builder_text_field.dart';
-
-import 'bottom_bar.dart';
 import 'dashboard.dart';
 import 'shared.dart';
 import 'widgets/side_drawer.dart';
@@ -30,6 +25,8 @@ class _DashboardDashboardPageState extends State<DashboardDashboardPage> {
   final navigationService = sl<NavigationService>();
   TextEditingController cityNameController = TextEditingController();
   FocusNode cityNameFocusNode = FocusNode();
+  File imgFile;
+  final _picker = ImagePicker();
 
 
   int currentTab = 0; // to keep track of active tab index
@@ -70,47 +67,143 @@ class _DashboardDashboardPageState extends State<DashboardDashboardPage> {
     ),
     floatingActionButton: FloatingActionButton(
     child: Icon(Icons.add),
-    onPressed: () {},
+    onPressed: () {
+      showOptionsDialog(context);
+    },
     ),
     floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    bottomNavigationBar: BottomBar(),
+    bottomNavigationBar: BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 10,
+      child: Container(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+                      currentScreen =
+                          Dashboard(); // if user taps on this dashboard tab will be active
+                      currentTab = 0;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.dashboard,
+                        color: currentTab == 0 ? Colors.blue : Colors.grey,
+                      ),
+                      Text(
+                        'Dashboard',
+                        style: TextStyle(
+                          color: currentTab == 0 ? Colors.blue : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            // Right Tab bar icons
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                MaterialButton(
+                  minWidth: 40,
+                  onPressed: () {
+                    setState(() {
+                      currentScreen =
+                          Shared(); // if user taps on this dashboard tab will be active
+                      currentTab = 2;
+                    });
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.dashboard,
+                        color: currentTab == 2 ? Colors.blue : Colors.grey,
+                      ),
+                      Text(
+                        'Profile',
+                        style: TextStyle(
+                          color: currentTab == 2 ? Colors.blue : Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    ),
     );
   }
 
-  void _showCityChangeDialog() {
-    showDialog(
+  Future<void> showOptionsDialog(BuildContext context) {
+    return showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Center(
-              child: Text(
-                changeCityText,
+            title: Text("Options"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text("Capture Image From Camera"),
+                    onTap: () {
+                      openCamera();
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(10)),
+                  GestureDetector(
+                    child: Text("Take Image From Gallery"),
+                    onTap: () {
+                      openGallery();
+                    },
+                  ),
+                ],
               ),
-            ),
-            actions: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
-                child: MyAppButton(
-                  text: okButtonText,
-                  onPressed: () {
-                    if (cityNameController.text.isEmpty) {
-                      BlocProvider.of<DashboardBloc>(context)..add(FetchDashboardForCurrentLocation());
-                    } else {
-                      BlocProvider.of<DashboardBloc>(context)
-                        ..add(FetchDashboardForGivenCity(cityName: cityNameController.text));
-                      FocusScope.of(context).unfocus();
-                    }
-                    Navigator.of(context).pop();
-                  },
-                ),
-              )
-            ],
-            content: MyAppFormBuilderTextField(
-              attribute: cityNameText,
-              label: cityNameText,
-              controller: cityNameController,
             ),
           );
         });
+  }
+
+  void openCamera() async {
+    var imgCamera = await _picker.getImage(source: ImageSource.camera);
+    setState(() {
+      imgFile = File(imgCamera.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  void openGallery() async {
+    var imgGallery = await _picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      imgFile = File(imgGallery.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  void uploadPicture(File imgFile) {
+    BlocProvider.of<DashboardBloc>(context).add(UploadImage(imgFile));
+  }
+  Widget displayImage(){
+    if(imgFile == null){
+      return Text("No Image Selected!");
+    } else{
+      return Image.file(imgFile, width: 350, height: 350);
+    }
   }
 }
--- lib/features/dashboard/presentation/pages/shared.dart
+++ lib/features/dashboard/presentation/pages/shared.dart
@@ -10,7 +10,7 @@ class _SharedState extends State<Shared> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Shareds'),
+        title: Text('Shared'),
       ),
     );
   }
